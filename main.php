c2f730be4464ef644fdb12f651cc27cf30e7f9fd
Zachary Rose <zachary.f.rose@gmail.com>
2015-11-25 00:18:25 -0700

    Added bandcamp players to main.php

--- main.php
+++ main.php
@@ -29,7 +29,18 @@
           <article>
                   <h1>Welcome to the Nekrofilth website!</h1>
                   <h2 style="margin-bottom:1em;">Not much to report on right now, but more will be here in the future.</h2>
-                  <h1><a href="store.php?directed=Main" class="greenlink">Check out the Store</a></h1>
+                  <h1 style="margin-bottom:1.5em; margin-right:0"><a href="store.php?directed=Main"  class="greenlink">Check out the Store</a></h1>
+
+                  <iframe style="border: 0; width: 50%; height: 7.5em; opacity: .75;" src="https://bandcamp.com/EmbeddedPlayer/album=1931631055/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://nekrofilth.bandcamp.com/album/gravehill-split">Gravehill Split by Nekrofilth</a></iframe>
+                  <iframe style="border: 0; width: 50%; height: 7.5em; opacity: .75;" src="https://bandcamp.com/EmbeddedPlayer/track=2243906709/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://nekrofilth.bandcamp.com/track/cross-me-fool-razor-cover">Cross Me Fool (Razor Cover) by Nekrofilth</a></iframe>
+                  <iframe style="border: 0; width: 50%; height: 7.5em; opacity: .75;" src="https://bandcamp.com/EmbeddedPlayer/album=4090019319/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://nekrofilth.bandcamp.com/album/filling-my-blood-with-poison">Filling My Blood With Poison... by Nekrofilth</a></iframe>
+                  <iframe style="border: 0; width: 50%; height: 7.5em; opacity: .75;" src="https://bandcamp.com/EmbeddedPlayer/album=891943977/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://nekrofilth.bandcamp.com/album/acid-brain">Acid Brain by Nekrofilth</a></iframe>
+                  <iframe style="border: 0; width: 50%; height: 7.5em; opacity: .75;" src="https://bandcamp.com/EmbeddedPlayer/album=3222928577/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://nekrofilth.bandcamp.com/album/devils-breath">Devil&#39;s Breath by Nekrofilth</a></iframe>
+                  <iframe style="border: 0; width: 50%; height: 7.5em; opacity: .75;" src="https://bandcamp.com/EmbeddedPlayer/album=727342420/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://nekrofilth.bandcamp.com/album/street-bitch">Street Bitch by Nekrofilth</a></iframe>
+                  <iframe style="border: 0; width: 50%; height: 7.5em; opacity: .75;" src="https://bandcamp.com/EmbeddedPlayer/album=2965211794/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://nekrofilth.bandcamp.com/album/shock-troopers">Shock Troopers by Nekrofilth</a></iframe>
+                  <iframe style="border: 0; width: 50%; height: 7.5em; opacity: .75;" src="https://bandcamp.com/EmbeddedPlayer/album=2208986352/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://nekrofilth.bandcamp.com/album/throne-of-sluts">Throne of Sluts by Nekrofilth</a></iframe>
+                  <iframe style="border: 0; width: 50%; height: 7.5em; opacity: .75;" src="https://bandcamp.com/EmbeddedPlayer/album=1581856411/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://nekrofilth.bandcamp.com/album/worship-destruction">Worship Destruction by Nekrofilth</a></iframe>
+
           </article>
 
 
